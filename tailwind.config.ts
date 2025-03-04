38dd228c5e5a4cc0e705162f8adbf602fce1e9dc
lbojo006 <luisbojorquezgoal@gmail.com>
2025-03-04 15:22:13 -0800

    Updated tailwind and global

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/data/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
-export default config;
